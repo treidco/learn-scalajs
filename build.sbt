d82e6f139a4fd48de9639e149119d4efeae4a751
Tim Reid <treid@spotxchange.com>
2017-04-14 22:25:47 +0100

    introduce the dom

--- build.sbt
+++ build.sbt
@@ -7,3 +7,5 @@ version := "1.0"
 scalaVersion := "2.12.1"
 
 scalaJSUseMainModuleInitializer := true
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.1"
--- src/main/scala/tutorial/Hello.scala
+++ src/main/scala/tutorial/Hello.scala
@@ -1,12 +1,22 @@
 package tutorial
 
+import org.scalajs.dom
+import org.scalajs.dom.document
+
 import scala.scalajs.js.JSApp
 
 object Hello extends JSApp {
 
   def main(): Unit = {
-    println("Hello!")
+    appendPar(document.body, "Hello!")
   }
 
+
+  def appendPar(target: dom.Node, text: String): Unit = {
+    val n = document.createElement("p")
+    val t = document.createTextNode(text)
+    n.appendChild(t)
+    target.appendChild(n)
+  }
 
 }
